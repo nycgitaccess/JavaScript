1bf4e379bfef543889ae6d57dff8e6811116ff03
ZohirRayhanMollah <rayhanzohir925@gmail.com>
2018-02-04 05:19:45 +0200

    Add files via upload

---
+++ week1/week1.js
@@ -0,0 +1,25 @@
+alert("you have successfully connect to the extranal js file");
+//step 1..
+let vari = "helloWorld";
+console.log(vari);
+//step 2
+let nearestWholeNumber = 4.7356;
+console.log(Math.ceil(nearestWholeNumber));
+//step 3...
+let colors =["red", "blue", "green"];
+//step 4..
+console.log(colors.length);
+
+//step 5....
+let x = "hehe";
+let y = 510;
+if(x === y){
+console.log("the variable created on the js that has same datatype");
+
+}else{
+console.log("the variable created on the js they got different datatype");
+}
+//step 6
+let m = 7;
+m = m % 3;
+console.log("the is the x value= " + m);
